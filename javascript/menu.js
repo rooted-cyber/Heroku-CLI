fdf3022209e3342767c52b066819823dfe399544
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-04-04 07:31:17 +0530

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,11 @@
+var lal = "\033[1;91m";
+var sada = "\033[0m";
+var suru = "[";
+var khatam = "]";
+var pila = "\033[1;93m"
+console.log(lal+suru+sada+1+lal+khatam+pila+ " Heroku login");
+console.log(lal+suru+sada+2+lal+khatam+pila+ " Heroku app list");
+console.log(lal+suru+sada+3+lal+khatam+pila+ " Check logs ");
+console.log(lal+suru+sada+4+lal+khatam+pila+ " Heroku apps restart");
+console.log(lal+suru+sada+5+lal+khatam+pila+ " Heroku apps info");
+console.log(lal+suru+sada+6+lal+khatam+pila+ " Exit");
---
+++ javascript/wlist.js
@@ -0,0 +1,3 @@
+var hara = "\033[1;92m";
+var sada = "\033[0m";
+console.log(hara + "Heroku app list \n\n" + sada);
